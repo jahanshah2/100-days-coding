f9fe7facd0fed104602b60dc49257b1eab9aec20
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 13:17:03 +0500

    Add files via upload

---
+++ 5 Question/Ques5.js
@@ -0,0 +1,4 @@
+"use strict";
+let user_name = "\t\n Jahan \t\n";
+console.log(user_name);
+console.log(user_name.trim());
---
+++ 5 Question/Ques5.ts
@@ -0,0 +1,7 @@
+let user_name:string ="\t\n Jahan \t\n";
+
+
+console.log(user_name);
+
+console.log(user_name.trim());
+
