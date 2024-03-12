5a4babb05ffc0269d319b5836f084d7542d0358d
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 14:22:35 +0500

    Add files via upload

---
+++ 8 Question/Ques8.js
@@ -0,0 +1,4 @@
+"use strict";
+let favoriteNumber = 8;
+let massage = `my favorite number is ${favoriteNumber}`;
+console.log(massage);
---
+++ 8 Question/Ques8.ts
@@ -0,0 +1,5 @@
+let favoriteNumber : number = 8;
+
+let massage: string = `my favorite number is ${favoriteNumber}`
+
+console.log(massage);
