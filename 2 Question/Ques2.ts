2a104a97c58647101d0a69bd4a805d52f29c80d6
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 12:07:51 +0500

    Add files via upload

---
+++ 2 Question/Ques2.ts
@@ -0,0 +1,9 @@
+var userName :string = "jahanshah "
+
+////////// ToUpperCase///////////
+console.log(userName.toUpperCase());
+////////// ToLowerCase//////////
+console.log(userName.toLowerCase());
+////////// ToTitleCase//////////
+console.log(userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase());
+
