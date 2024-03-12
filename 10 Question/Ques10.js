65c63457ecc6f63888dfb3812be00eb59e4f1a4a
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 14:57:35 +0500

    Add files via upload

---
+++ 10 Question/Ques10.js
@@ -0,0 +1,5 @@
+"use strict";
+var studentsName = ["Hamza", "Ali", "Faisal", "Jaweed", "ShahJahan", "ShahMeer"];
+for (let i = 0; i < studentsName.length; i++) {
+    console.log(studentsName[i]);
+}
---
+++ 10 Question/Ques10.ts
@@ -0,0 +1,8 @@
+var studentsName :string []=["Hamza","Ali","Faisal","Jaweed","ShahJahan","ShahMeer"]
+
+for (let i = 0; i < studentsName.length; i++) {
+    
+    console.log(studentsName[i]);
+    
+}
+    
