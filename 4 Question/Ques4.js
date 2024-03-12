28375bcff0a4c80a05d6ea815d863dd394bb47cb
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 12:53:02 +0500

    Add files via upload

---
+++ 4 Question/Ques4.js
@@ -0,0 +1,4 @@
+"use strict";
+let user_name = "Albert Einstein";
+let massege = `${user_name},once said, “A person who never made a mistake never tried anything new.”`;
+console.log(massege);
---
+++ 4 Question/Ques4.ts
@@ -0,0 +1,5 @@
+let user_name :string = "Albert Einstein";
+
+let massege:string = `${user_name},once said, “A person who never made a mistake never tried anything new.”`;
+
+console.log(massege);
