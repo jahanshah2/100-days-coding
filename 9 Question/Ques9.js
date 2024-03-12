87163e9e5a86ca47e91610a488d0950ced8b927b
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 14:40:52 +0500

    Add files via upload

---
+++ 9 Question/Ques9.js
@@ -0,0 +1,6 @@
+"use strict";
+// myName "Jahan Shah" //Current Date 3/12/2024
+// This Programe is not login/sign up and other work this is only my favorite number print    
+let num1 = 69;
+let massege = `This is my favorite number ${num1}`;
+console.log(massege);
---
+++ 9 Question/Ques9.ts
@@ -0,0 +1,7 @@
+// myName "Jahan Shah" //Current Date 3/12/2024
+// This Programe is not login/sign up and other work this is only my favorite number print    
+let num1 : number = 69;
+
+let massege :string = `This is my favorite number ${num1}`
+
+console.log(massege);
