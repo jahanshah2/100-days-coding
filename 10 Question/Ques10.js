e8b1ea62c52768bbfc9a1b9711aa671ae63e6d03
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 14:58:33 +0500

    Delete 10 Question directory

--- 10 Question/Ques10.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var studentsName = ["Hamza", "Ali", "Faisal", "Jaweed", "ShahJahan", "ShahMeer"];
-for (let i = 0; i < studentsName.length; i++) {
-    console.log(studentsName[i]);
-}
--- 10 Question/Ques10.ts
+++
@@ -1,8 +0,0 @@
-var studentsName :string []=["Hamza","Ali","Faisal","Jaweed","ShahJahan","ShahMeer"]
-
-for (let i = 0; i < studentsName.length; i++) {
-    
-    console.log(studentsName[i]);
-    
-}
-    
