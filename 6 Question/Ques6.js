ba263d8a8f4ac52bc866302df6564bcc2785506e
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-12 14:10:04 +0500

    Add files via upload

---
+++ 6 Question/Ques6.js
@@ -0,0 +1,6 @@
+"use strict";
+let addition = 3 + 5;
+let subtrection = 10 - 2;
+let multiplication = 4 * 2;
+let division = 16 / 2;
+console.log(addition, subtrection, multiplication, division);
---
+++ 6 Question/Ques6.ts
@@ -0,0 +1,9 @@
+let addition: number = 3 + 5;
+
+let subtraction: number = 10 - 2;
+
+let multiplication: number = 4 * 2;
+
+let division: number = 16 / 2;
+
+console.log(addition,subtraction,multiplication,division);
