cb3b1dc7edd0c7f031ea0eb05dc9da3f60a0cbca
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-13 12:04:26 +0500

    Add files via upload

---
+++ 11 Question/Ques11.js
@@ -0,0 +1,5 @@
+"use strict";
+let Names = ["saleem", "shahzaib", "jaweed", "shahmeer", "jon"];
+for (let name of Names) {
+    console.log(`Hellow dear friend ${name}, you are the great person `);
+}
---
+++ 11 Question/Ques11.ts
@@ -0,0 +1,7 @@
+let Names:string[]=["saleem","shahzaib","jaweed","shahmeer","jon"]
+
+for(let name of Names){
+
+console.log(`Hellow dear friend ${name}, you are the great person `)
+
+}
