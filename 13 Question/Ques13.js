e2c0f70b7e0404365c8e21e7105b74e5684a9c39
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-13 16:19:47 +0500

    Add files via upload

---
+++ 13 Question/Ques13.js
@@ -0,0 +1,5 @@
+"use strict";
+let guests = ["Faraz", "Noman", "Waseem"];
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
---
+++ 13 Question/Ques13.ts
@@ -0,0 +1,7 @@
+let guests :string []=["Faraz","Noman","Waseem"]
+
+guests.forEach(guest =>{
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+    
+})
+
